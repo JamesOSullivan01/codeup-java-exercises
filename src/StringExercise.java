e7ed69b433521c565c37fdb4717c949e46ea00e3
James OSullivan <james.a.osullivan01@gmail.com>
2023-06-15 13:46:01 -0500

    Completed string basics questions.

---
+++ src/StringExercise.java
@@ -0,0 +1,21 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        // For each of the following output examples: create a String variable that contains the desired output and print it out to the console, you can do this with only one String variable and one print statement for each output example.
+
+        String pinkFloyd = "We don't need no education \nWe don't need no though control";
+        System.out.println(pinkFloyd);
+
+        String quoteString = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(quoteString);
+
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        String backslashBonanza = "I can do backslashes \\, doouble backslashes \\\\ , and the amazing triple backslash\\\\\\!";
+        System.out.println("backslashBonanza = " + backslashBonanza);
+
+
+
+    }
+}
