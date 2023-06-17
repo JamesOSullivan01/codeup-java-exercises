84c7bd020c6f585c2dfef7df24c795d35834ed01
James OSullivan <james.a.osullivan01@gmail.com>
2023-06-17 10:08:59 -0500

    Added a guess limit and a line to shw how many guesses it toook them to win.

--- src/HighLow.java
+++ src/HighLow.java
@@ -9,19 +9,26 @@ public class HighLow {
         System.out.println("Please pick a number between 1 and 100.");
 
         boolean guessedCorrectly = false;
+        int count = 20;
+        int numOfGuesses = 0;
 
         do {
             int userInput = scanner.nextInt();
 
             if (userInput < randomNumber) {
                 System.out.println("Your number is too low, guess again.");
+                count--;
+                numOfGuesses++;
             } else if (userInput > randomNumber) {
                 System.out.println("Your number is too high, guess again.");
+                count--;
+                numOfGuesses++;
             } else {
                 System.out.println("Congratulations, you won!");
+                System.out.println("It took you " + numOfGuesses + " guesses to win");
                 guessedCorrectly = true;
             }
-
+            System.out.println("You have " + count + " guesses remaining");
         } while (!guessedCorrectly);
     }
     public static void main(String[] args) {
